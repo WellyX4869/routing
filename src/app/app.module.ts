f3d7ecfa958886d9015c2c40555d47a44e64dd21
WellyX4869 <wellysusanto4869@gmail.com>
2020-04-23 18:10:45 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { LoginComponent} from './login/login.component';
-import { TestComponent } from './test/test.component';
 import { Routes, RouterModule } from '@angular/router';
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
@@ -13,9 +11,5 @@ import { Routes, RouterModule } from '@angular/router';
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { 
-  const ROUTES: Routes ={
-    {path: '', component: HomeComponent},
-    {path: 'login', component: LoginComponent}
-    {path: 'test', component: TestComponent}
-  }  
+ 
 }
